45191c73fb48812234697366d4d1232f793aaa2c
YoNyeoSeok <yys8646@postech.ac.kr>
2018-09-17 00:43:11 +0900

    get, set setting

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import tensorflow as tf
 import sklearn
+from model import simple
 
 class Run():
     def __init__(self, spec):
+        # model, data, checkpoint, max_iter, batch_size, optimizer, lr, interval, random_seed
         print(spec)
         if spec[0] == 'train':
             pass
@@ -15,3 +17,15 @@ class Run():
     def svm(self):
        pass 
 
+    def logistic(self):
+        tf.reset_default_graph()
+        with tf.Session() as sess:
+            simple.load_model(sess,
+                    dataset,
+                    epochs,
+                    classes,
+                    step_interval,
+                    image_size,
+                    train,
+                    test
+                    )
