9eaf9d9425d64c0d12557d2e23727a867dc2b867
mlg <YoNyeoSeok>
2018-11-27 15:32:58 +0900

    setup dependencies

--- setup.py
+++ setup.py
@@ -2,6 +2,13 @@ from setuptools import setup
 import os
 
 setup(name='TensorflowGUI',
-      version='0.1',
-      packages=['model', 'icons', 'utils', 'gui'],
-      scripts=['bin/TensorflowGUI'])
+    version='0.1',
+    packages=['model', 'icons', 'utils', 'gui'],
+    scripts=['bin/TensorflowGUI'],
+    install_requires=[
+        'tensorflow==1.8',
+        'wxpython', 
+        'Pillow',
+        'matplotlib',
+        'sklearn'],
+    python_requires='>3')
