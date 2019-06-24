33c71ae9eedf6cbd9eaf4fd571ee02cdca4f026e
YoNyeoSeok <yys8646@postech.ac.kr>
2019-06-24 17:54:23 +0900

    add model guide

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ python app.py
 ## Make Example Datasets
 mnist: python dataset/mnist.py
 cifar10: python dataset/cifar10.py
+
+## add model guide
+See add\_model\_guide.txt
--- model/Xception/__init__.py
+++ model/Xception/__init__.py
@@ -1,10 +1,1 @@
 from Xception import build, train, test
-# from treelib import Tree
-# from pathlib import Path
-# import os
-
-# Xception_TREE = Tree()
-# Xception_TREE.create_node(
-#     'Xception',
-#     Path(Xception.__path__[0]).relative_to(os.getcwd()),
-#     data=Xception)
