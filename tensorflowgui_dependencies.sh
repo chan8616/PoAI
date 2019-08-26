4bd241625d035186854603a7c01b90c894916678
yys8646 <yys8646@postech.ac.kr>
2019-08-26 02:15:10 +0000

    ADD dependencies.sh

---
+++ tensorflowgui_dependencies.sh
@@ -0,0 +1,6 @@
+# pip
+pip install wxpython  # ref dependencies
+pip install treelib
+pip install gooey
+pip install keras
+pip install tensorflow  # tensorboard, setuptools version error
---
+++ wxpython_dependencies.sh
@@ -0,0 +1,18 @@
+# apt
+apt-get install -y dpkg-dev
+apt-get install -y build-essential
+apt-get install -y python3.6-dev
+apt-get install -y libjpeg-dev
+apt-get install -y libtiff-dev
+apt-get install -y libsdl1.2-dev
+apt-get install -y libgstreamer-plugins-base1.10-dev
+apt-get install -y libnotify-dev
+apt-get install -y freeglut3
+apt-get install -y freeglut3-dev
+apt-get install -y libsm-dev
+apt-get install -y libgtk-3-dev
+apt-get install -y libwebkitgtk-3.0-dev
+
+# pip
+pip install pathlib2
+pip install wxPython
