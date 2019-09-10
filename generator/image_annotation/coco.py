c454d75446cd8571af1d3fdbb03f005e490cdbee
yys8646 <yys8646@postech.ac.kr>
2019-09-10 09:31:51 +0000

    RENAME image_annotations to image_annotation

--- generator/image_annotation/coco.py
+++ generator/image_annotation/coco.py
@@ -402,37 +402,44 @@ def evaluate_coco(model, dataset, coco, eval_type="bbox", limit=0, image_ids=Non
 
 def coco_parser(parser: GooeyParser = GooeyParser()) -> GooeyParser:
     # Parse command line arguments
-    parser.add_argument("command",
-                        choices=['train', 'evaluate'],
-                        metavar="<command>",
-                        help="'train' or 'evaluate' on MS COCO")
+    #  parser.add_argument("command",
+    #                      choices=['train', 'evaluate'],
+    #                      metavar="<command>",
+    #                      help="'train' or 'evaluate' on MS COCO")
     parser.add_argument('--dataset', required=True,
                         metavar="/path/to/coco/",
+                        default='val2014_/',
                         help='Directory of the MS-COCO dataset')
-    parser.add_argument('--year', required=False,
-                        default=DEFAULT_DATASET_YEAR,
-                        metavar="<year>",
-                        help='Year of the MS-COCO dataset (2014 or 2017)'
-                        ' (default=2014)')
-    parser.add_argument('--model', required=True,
-                        metavar="/path/to/weights.h5",
-                        help="Path to weights .h5 file or 'coco'")
-    parser.add_argument('--logs', required=False,
-                        default=DEFAULT_LOGS_DIR,
-                        metavar="/path/to/logs/",
-                        help='Logs and checkpoints directory (default=logs/)')
-    parser.add_argument('--limit', required=False,
-                        default=500,
-                        metavar="<image count>",
-                        help='Images to use for evaluation (default=500)')
-    parser.add_argument('--download', required=False,
-                        default=False,
-                        metavar="<True|False>",
-                        help='Automatically download and unzip MS-COCO files'
-                        ' (default=False)',
-                        type=bool)
+    #  parser.add_argument('--year', required=False,
+    #                      default=DEFAULT_DATASET_YEAR,
+    #                      metavar="<year>",
+    #                      help='Year of the MS-COCO dataset (2014 or 2017)'
+    #                      ' (default=2014)')
+    #  parser.add_argument('--model', required=True,
+    #                      metavar="/path/to/weights.h5",
+    #                      help="Path to weights .h5 file or 'coco'")
+    #  parser.add_argument('--logs', required=False,
+    #                      default=DEFAULT_LOGS_DIR,
+    #                      metavar="/path/to/logs/",
+    #                      help='Logs and checkpoints directory (default=logs/)')
+    #  parser.add_argument('--limit', required=False,
+    #                      default=500,
+    #                      metavar="<image count>",
+    #                      help='Images to use for evaluation (default=500)')
+    #  parser.add_argument('--download', required=False,
+    #                      default=False,
+    #                      metavar="<True|False>",
+    #                      help='Automatically download and unzip MS-COCO files'
+    #                      ' (default=False)',
+    #                      type=bool)
     return parser
 
+
+def coco(args):
+    file_names = next(os.walk(IMAGE_DIR))[2]
+    image = skimage.io.imread(os.path.join(
+        IMAGE_DIR, random.choice(file_names)))
+    return image
 
 ############################################################
 #  Training
