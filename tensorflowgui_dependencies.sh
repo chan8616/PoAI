04f5265793f78fbb9e63b93285a8a7662aef05d7
yys8646 <yys8646@postech.ac.kr>
2019-10-04 00:41:35 +0000

    Update dependencies

--- tensorflowgui_dependencies.sh
+++ tensorflowgui_dependencies.sh
@@ -1,9 +1,13 @@
 # pip
 pip install wxpython  # ref dependencies
 pip install treelib
-pip install gooey
-pip install keras
-pip install tensorflow  # tensorboard, setuptools version error
+pip install gooey==1.0.1
+pip install keras==2.1.6
+pip install numpy==1.16.4
+pip install tensorflow==1.12.0  # tensorboard, setuptools version error
+
+# svm
+pip install scikit-learn
 
 # pycocotools
 pip install Cython
@@ -12,3 +16,6 @@ pip install pycocotools
 # image
 pip install pandas
 pip install xlsxwriter
+
+# Mask RCNN
+pip install imgaug
