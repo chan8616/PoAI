0bafb60b884b95254796d77d6312cb83b4d20d1b
yys8646 <yys8646@postech.ac.kr>
2019-10-21 05:44:11 +0000

    Update 'TF_'WIEGHTS_NO_TOP typo

--- model/ResNet/ResNet50/model.py
+++ model/ResNet/ResNet50/model.py
@@ -5,7 +5,7 @@ from keras.applications.resnet50 import ResNet50  # type: ignore
 from model.model_config import ModelConfig
 from model.keras_applications import model as modellib
 
-WEIGHTS_PATH_NO_TOP = (
+TF_WEIGHTS_PATH_NO_TOP = (
         'https://github.com/fchollet/deep-learning-models/releases/download/'
         'v0.2/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5')
 
