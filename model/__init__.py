da7d584c0aa79681bcfdef53deae544eabb210b7
yys8646 <yys8646@postech.ac.kr>
2019-11-19 11:21:11 +0000

    Update Logistic model

--- model/__init__.py
+++ model/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['Mask_RCNN', 'BERT', 'MobileNet', 'Xception', 'InceptionV3', 'VGG', 'ResNet', 'DenseNet']
+__all__ = ['Mask_RCNN', 'BERT', 'MobileNet', 'Xception', 'InceptionV3', 'VGG', 'ResNet', 'DenseNet', 'Logistic']
