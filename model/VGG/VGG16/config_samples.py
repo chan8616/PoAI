1b9ea0a6230918f1010260d2a53968edb0669ec6
YoNyeoSeok <yys8646@postech.ac.kr>
2019-12-01 13:35:24 +0000

    Update VGG16 OlivettiFaces

--- model/VGG/VGG16/config_samples.py
+++ model/VGG/VGG16/config_samples.py
@@ -2,7 +2,11 @@ import os
 
 from model.keras_applications.train_config import (
         WEIGHTS, LOSSES, OPTIMIZERS)
-from generator.image_classification.config_samples import DIR_GEN_CIFAR10
+from generator.image_classification.config_samples \
+        import (DIR_GEN_CIFAR10,
+                DIR_GEN_OlivettiFaces,
+                COLOR_MODES,
+                )
 from .train_config import VGG16TrainConfig
 from .build_config import VGG16Config, POOLINGS
 
@@ -43,3 +47,32 @@ class VGG16CIFAR10Config(
         super(VGG16CIFAR10Config, self).__init__()
         self.CLASSES = len(self.LABELS)
         self.TARGET_SIZE = self.INPUT_SHAPE[:2]
+
+
+class VGG16OlivettiFacesConfig(
+        VGG16Config,
+        VGG16TrainConfig,
+        DIR_GEN_OlivettiFaces,
+        ):
+    NAME = os.path.join(VGG16Config.NAME, DIR_GEN_OlivettiFaces.NAME)
+
+    INPUT_SHAPE = (64, 64, 3)  # type: ignore
+
+    POOLINGS = POOLINGS[0]
+    HIDDEN_LAYERS = [256, 256]
+
+    WEIGHT = WEIGHTS[0]  # type: ignore
+    EPOCHS = 20
+    #  VALIDATION_STEPS = 10
+
+    LOSS = LOSSES[2]
+
+    OPTIMIZER = OPTIMIZERS[1]
+    LEARNING_RATE = 1e-4
+    LEARNING_MOMENTUM = 0.0
+
+    def __init__(self):
+        super(VGG16OlivettiFacesConfig, self).__init__()
+        self.CLASSES = len(self.LABELS)
+        self.TARGET_SIZE = self.INPUT_SHAPE[:2]
+        self.COLOR_MODE = COLOR_MODES[1]
--- model/VGG/VGG16/generator.py
+++ model/VGG/VGG16/generator.py
@@ -10,6 +10,7 @@ from model.keras_applications.generator_config_samples \
 
 from .config_samples import (
         VGG16CIFAR10Config,
+        VGG16OlivettiFacesConfig,
         )
 
 
@@ -24,6 +25,7 @@ def generator_parser(
                 ImageClassificationGeneratorConfig()),
             directory_dataset_config=DirectoryDatasetConfig(),
             dataset_generator_configs=OrderedDict([
+                ('directory_olivetti_faces', VGG16OlivettiFacesConfig()),
                 ('directory_cifar10', VGG16CIFAR10Config()),
                 #  'directory_cifar100': DGC_CIFAR100(),
                 #  'directory_mnist': DGC_MNIST(),
