36203124f52e8cfd5c651d54046ff34ec6417706
YoNyeoSeok <yys8646@postech.ac.kr>
2019-12-01 15:21:29 +0000

    Fix segmentation fault!

--- gui/utils.py
+++ gui/utils.py
@@ -1,10 +1,12 @@
+import wx
+
 
 class Redirection(object):
     def __init__(self, log_area):
         self.out = log_area
 
     def write(self, string):
-        self.out.WriteText(string)
+        wx.CallAfter(self.out.WriteText, string)
 
     def flush(self):
         pass
